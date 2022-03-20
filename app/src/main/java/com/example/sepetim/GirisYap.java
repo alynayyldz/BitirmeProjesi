20fa91fc436f4497c20fd408dd90879c8fe7a3fd
Aleyna Ayyıldız <alynayyldz@gmail.com>
2022-03-20 23:14:24 +0300

    Genel Geliştirmeler

--- app/src/main/java/com/example/sepetim/GirisYap.java
+++ app/src/main/java/com/example/sepetim/GirisYap.java
@@ -1,54 +1,70 @@
 package com.example.sepetim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.HashMap;
-import java.util.Map;
 
 public class GirisYap extends AppCompatActivity {
 
-    Button girisYap;
-    EditText editGirisMail, editGirisSifre;
+    private EditText editGirisMail, editGirisSifre;
+    private String txtgirisMail, txtgirissifre;
 
-
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giris_yap);
 
-        girisYap = (Button) findViewById(R.id.girisYap);
+
         editGirisMail = (EditText) findViewById(R.id.editGirisMail);
         editGirisSifre = (EditText) findViewById(R.id.editGirisSifre);
 
+        mAuth = FirebaseAuth.getInstance();
 
-        girisYap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               
-            }
-        });
+    }
+    public void girisyap(View view){
+        txtgirisMail = editGirisMail.getText().toString();
+        txtgirissifre = editGirisSifre.getText().toString();
+
+        if(!TextUtils.isEmpty(txtgirisMail) && !TextUtils.isEmpty(txtgirissifre)){
+            mAuth.signInWithEmailAndPassword(txtgirisMail,txtgirissifre).addOnSuccessListener(this, new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    mUser = mAuth.getCurrentUser();
+                    Intent intent = new Intent(getApplicationContext(),AnaSayfa.class);
+                    startActivity(intent);
+                    Toast.makeText(GirisYap.this,"Giriş Başarılı!",Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(this, new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(GirisYap.this,"Şifre veya Email Hatalı!",Toast.LENGTH_SHORT).show();
+                }
+            });
+        } else {
+            Toast.makeText(GirisYap.this,"Şifre ya da e-mail boş bırakalamaz.",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
--- app/src/main/java/com/example/sepetim/KayitOl.java
+++ app/src/main/java/com/example/sepetim/KayitOl.java
@@ -1,54 +1,66 @@
 package com.example.sepetim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class KayitOl extends AppCompatActivity {
 
-    Button kayitOl;
-    EditText editAd, editTelefon, editEmail, editSifre;
 
+    private EditText editAd,editSoyad,editTelefon,editEmail,editSifre,editSifreOnay;
+    private String ad,soyad,txtEmail,telefon,txtSifre,sifreOnay;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kayit_ol);
 
-        kayitOl = (Button) findViewById(R.id.kayitOl);
-        editAd = (EditText) findViewById(R.id.editAd);
-        editTelefon = (EditText) findViewById(R.id.editTelefon);
-        editEmail = (EditText) findViewById(R.id.editEmail);
-        editSifre = (EditText) findViewById(R.id.editSifre);
+        editEmail=(EditText) findViewById(R.id.editEmail);
+        editSifre=(EditText) findViewById(R.id.editSifre);
+        editAd=(EditText) findViewById(R.id.editAd);
+        editTelefon=(EditText) findViewById(R.id.editTelefon);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
 
 
-        kayitOl.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+    public void kayitbass(View v){
+        txtEmail=editEmail.getText().toString();
+        txtSifre=editSifre.getText().toString();
 
-            }
-        });
+        if(!TextUtils.isEmpty(txtEmail) && !TextUtils.isEmpty(txtSifre)){
+            mAuth.createUserWithEmailAndPassword(txtEmail,txtSifre)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                                Toast.makeText(KayitOl.this,"Kayıt işlemi Başarılı", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(KayitOl.this, "Bu e-mail kayıtlı.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+        }else
+            Toast.makeText(this, "E-mail Ve Şifre Boş Olamaz", Toast.LENGTH_SHORT).show();
+
+
+
+
+
     }
 }
-
